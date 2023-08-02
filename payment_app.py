dead62235dcb0e811137165dad1d915cff36b1a6
Tuleninov Roman <tuleninovroman@icloud.com>
2023-08-02 12:38:13 +0300

    feat: add dialoge window

--- payment_app.py
+++ payment_app.py
@@ -277,12 +277,6 @@ def new_payment():
         cursor.execute("SELECT contractor_name FROM our_companies")
         our_companies = cursor.fetchall()
 
-        cursor.execute(
-            "SELECT nextval(pg_get_serial_sequence('payments_summary_tab', 'payment_id'))")
-        # "SELECT payment_id FROM payments_summary_tab ORDER BY payment_id DESC LIMIT 1")
-        last_payment_id = cursor.fetchall()
-        print(last_payment_id[0][0])
-
         # Close the database connection
         coon_cursor_close(cursor, conn)
 
@@ -309,17 +303,11 @@ def new_payment_save_data():
             #     # session['submitted'] = False
             #     return redirect(url_for('new_payment'))
 
-
-
-
             # Get the form data from the request
             basis_of_payment = request.form.get('basis_of_payment')
-            responsible = request.form.get('responsible')
+            responsible = request.form.get('responsible_name')
             cost_items = request.form.get('cost_items').split('-@@@-')[1]
-            try:
-                object_id = request.form.get('objects_name')
-            except:
-                object_id = None
+            object_id = request.form.get('objects_name')
             payment_description = request.form.get('payment_description')
             partner = request.form.get('partners')
             payment_due_date = request.form.get('payment_due_date')
@@ -327,17 +315,10 @@ def new_payment_save_data():
             payment_sum = request.form.get('payment_sum')
             payment_sum = payment_sum.replace(' руб.', '').replace(" ", "").replace(",", ".")
 
+
             for key, value in request.form.items():
                 print(f"Форма: {key}, Значение: {value}")
-
-            # Connect to the database
-            conn, cursor = coon_cursor_init()
-
-
-
-            # return redirect(url_for('new_payment'))
-            #
-            # print(our_company)
+            print(our_company)
             print(payment_sum)
 
             # Connect to the database
@@ -345,13 +326,6 @@ def new_payment_save_data():
 
             cursor.execute(
                 "SELECT nextval(pg_get_serial_sequence('payments_summary_tab', 'payment_id'))")
-            # "SELECT payment_id FROM payments_summary_tab ORDER BY payment_id DESC LIMIT 1")
-            last_payment_id = cursor.fetchall()
-
-            # cursor.execute(
-            #     "SELECT payment_id FROM payments_summary_tab ORDER BY payment_id DESC LIMIT 1")
-            cursor.execute(
-                "SELECT payment_id FROM payments_summary_tab ORDER BY payment_id DESC LIMIT 1")
             last_payment_id = cursor.fetchall()
             if not last_payment_id:
                 last_payment_id = 1
@@ -401,8 +375,6 @@ def new_payment_save_data():
                 current_user.get_id(),
                 responsible)
 
-            pprint(values_s_t)
-
             # Prepare the SQL query to insert the data into the payments_andrew_statuses
             query_a_s = """
             INSERT INTO payments_andrew_statuses (
@@ -445,7 +417,7 @@ def new_payment_save_data():
                 # Close the database connection
                 coon_cursor_close(cursor, conn)
 
-                flash(f' Платёж НЕ сохранён \\n{str(e)}', category='error')
+                flash(['Платёж НЕ сохранён', str(e)], category='error')
                 return redirect(url_for('new_payment'))
         return redirect(url_for('new_payment'))
 
@@ -864,9 +836,7 @@ def logout():
             "name": ["Вы используете гостевой доступ", '(Войти)'], "url": "login"}
 
         # return redirect(url_for('login'))
-        # return render_template('new_contr.html', menu=hlnk_menu, menu_profile=hlnk_profile, title='Новый договор 📝')
-        return render_template('index.html', menu=hlnk_menu, menu_profile=hlnk_profile, title='Новый договор 📝')
-        return index()
+        return render_template('new_contr.html', menu=hlnk_menu, menu_profile=hlnk_profile, title='Новый договор 📝')
     except Exception as e:
         return f'logout ❗❗❗ Ошибка \n---{e}'
 
@@ -1004,8 +974,8 @@ def func_hlnk_profile():
                  "img": "https://cdn-icons-png.flaticon.com/512/6489/6489329.png"},
                 {"name": "Новый платеж", "url": "new_payment",
                  "img": "https://cdn-icons-png.flaticon.com/512/5776/5776429.png"},
-                # {"name": "Авторизация", "url": "login",
-                #  "img": "https://cdn-icons-png.flaticon.com/512/2574/2574003.png"},
+                {"name": "Авторизация", "url": "login",
+                 "img": "https://cdn-icons-png.flaticon.com/512/2574/2574003.png"},
             ]
 
         return
