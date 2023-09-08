ecdf4cdcf37057a730875abf3a5eb2b07372d919
dut1k <nikola.88@list.ru>
2023-09-08 23:54:52 +0300

    "feat: login_app change database configuration"

--- payment_app.py
+++ payment_app.py
@@ -278,6 +278,7 @@ def get_unapproved_payments():
                     t1.payment_number,  
                     SUBSTRING(t1.basis_of_payment, 1,70) AS basis_of_payment_short,
                     t1.basis_of_payment, 
+                    t5.user_id,
                     t5.first_name,
                     t5.last_name,
                     SUBSTRING(t1.payment_description, 1,70) AS payment_description_short,
@@ -1656,164 +1657,17 @@ def save_payment():
     return jsonify({'message': 'Payment saved successfully'})
 
 
-# @payment_app_bp.route('/get_modal_payment/<payment_id>')
-# def get_modal_payment(payment_id):
-@payment_app_bp.route('/get_modal_payment')
-def get_modal_payment():
-    try:
-        user_id = login_app.current_user.get_id()
-        # Connect to the database
-        conn, cursor = login_app.conn_cursor_init_dict()
-        payment_id = 1
-        # Список платежей со статусом "new"
-        cursor.execute(
-            """SELECT 
-                    t1.payment_id,
-                    t3.contractor_name, 
-                    t3.contractor_id, 
-                    t4.cost_item_name, 
-                    t1.payment_number,  
-                    SUBSTRING(t1.basis_of_payment, 1,70) AS basis_of_payment_short,
-                    t1.basis_of_payment, 
-                    t5.first_name,
-                    t5.last_name,
-                    SUBSTRING(t1.payment_description, 1,70) AS payment_description_short,
-                    t1.payment_description, 
-                    COALESCE(t6.object_name, '') AS object_name,
-                    t1.partner,
-                    t1.payment_sum,
-                    COALESCE(TRIM(to_char(t1.payment_sum, '999 999 999D99 ₽')), '') AS payment_sum_rub,
-                    COALESCE(t1.payment_sum - t2.approval_sum, t1.payment_sum) AS approval_sum,
-                    TRIM(to_char(COALESCE(t1.payment_sum - t2.approval_sum, t1.payment_sum), '9 999 999D99 ₽')) AS approval_sum_rub,
-                    COALESCE(t8.amount, null) AS amount,
-                    COALESCE(TRIM(to_char(t8.amount, '999 999 999D99 ₽')), '') AS amount_rub,
-                    t1.payment_due_date AS payment_due_date2,
-                    to_char(t1.payment_due_date, 'yy.mm.dd') AS payment_due_date,
-                    t2.status_id,
-                    date_trunc('second', timezone('UTC-3', t1.payment_at)::timestamp) AS payment_at,
-                    t1.payment_full_agreed_status
-            FROM payments_summary_tab AS t1
-            LEFT JOIN (
-                    SELECT DISTINCT ON (payment_id) 
-                        payment_id,
-                        status_id,
-                        SUM(approval_sum) OVER (PARTITION BY payment_id) AS approval_sum
-                    FROM payments_approval_history
-                    ORDER BY payment_id, create_at DESC
-            ) AS t2 ON t1.payment_id = t2.payment_id
-            LEFT JOIN (
-                SELECT contractor_id,
-                    contractor_name
-                FROM our_companies            
-            ) AS t3 ON t1.our_companies_id = t3.contractor_id
-            LEFT JOIN (
-                SELECT cost_item_id,
-                    cost_item_name
-                FROM payment_cost_items            
-            ) AS t4 ON t1.cost_item_id = t4.cost_item_id
-            LEFT JOIN (
-                    SELECT user_id,
-                        first_name,
-                        last_name
-                    FROM users
-            ) AS t5 ON t1.responsible = t5.user_id
-            LEFT JOIN (
-                    SELECT object_id,
-                        object_name
-                    FROM objects
-            ) AS t6 ON t1.object_id = t6.object_id
-            LEFT JOIN (
-                    SELECT DISTINCT ON (payment_id) 
-                        parent_id::int AS payment_id,
-                        parameter_value::float AS amount
-                    FROM payment_draft
-                    WHERE page_name = %s AND parameter_name = %s AND user_id = %s
-                    ORDER BY payment_id, create_at DESC
-            ) AS t8 ON t1.payment_id = t8.payment_id
-            WHERE not t1.payment_close_status AND t1.payment_id = %s
-            ORDER BY t1.payment_due_date;
-            """,
-            ['payment-approval', 'amount', user_id, payment_id]
-        )
-        payment = cursor.fetchall()
-        pprint('get_modal_payment')
-        pprint(payment)
-
-        cursor.execute(
-            """WITH
-                t0 AS (SELECT 
-                    payment_id,
-                    SUM(approval_sum) AS approval_sum
-                          FROM payments_approval_history
-                          GROUP BY payment_id)
-            SELECT 
-                    t1.payment_id,
-                    date_trunc('second', timezone('UTC-3', t1.create_at)::timestamp) AS payment_at,
-                    t2.payment_agreed_status_name, 
-                    t0.approval_sum,
-                    TRIM(to_char(t1.approval_sum, '9 999 999D99 ₽')) AS approval_sum_rub
-            FROM payments_approval_history AS t1
-            LEFT JOIN (
-                    SELECT  
-                        payment_agreed_status_id,
-                        payment_agreed_status_name
-                    FROM payment_agreed_statuses
-            ) AS t2 ON t1.status_id = t2.payment_agreed_status_id
-            LEFT JOIN t0 ON t1.payment_id = t0.payment_id
-
-            WHERE t1.payment_id = %s
-            ORDER BY t1.create_at;
-            """,
-            [payment_id]
-        )
-        approval = cursor.fetchall()
-
-        cursor.execute(
-            """WITH
-                t0 AS (SELECT 
-                    payment_id,
-                    SUM(paid_sum) AS paid_sum
-                          FROM payments_paid_history
-                          GROUP BY payment_id)
-            SELECT 
-                    t1.payment_id,
-                    date_trunc('second', timezone('UTC-3', t1.create_at)::timestamp) AS payment_at,
-                    t2.payment_agreed_status_name, 
-                    t0.paid_sum,
-                    TRIM(to_char(t1.paid_sum, '9 999 999D99 ₽')) AS paid_sum_rub
-            FROM payments_paid_history AS t1
-            LEFT JOIN (
-                    SELECT  
-                        payment_agreed_status_id,
-                        payment_agreed_status_name
-                    FROM payment_agreed_statuses
-            ) AS t2 ON t1.status_id = t2.payment_agreed_status_id
-            LEFT JOIN t0 ON t1.payment_id = t0.payment_id
-
-            WHERE t1.payment_id = %s
-            ORDER BY t1.create_at;
-            """,
-            [payment_id]
-        )
-        paid = cursor.fetchall()
-
-        login_app.conn_cursor_close(cursor, conn)
-        # payment = jsonify(payment)
-        # pprint(payment)
-        return render_template('application_modal.html', paid=paid)
-    except Exception as e:
-        return f'save_quick_changes_approved_payments ❗❗❗ Ошибка \n---{e}'
-
-
 @payment_app_bp.route('/update_payment', methods=['POST'])
 def update_payment():
     # Get the data from the AJAX request
     data = request.get_json()
 
+    print('data  ', data)
+
     user_id = login_app.current_user.get_id()
     # Connect to the database
     conn, cursor = login_app.conn_cursor_init_dict()
-    payment_id = 1
+    payment_id = data['paymentId']
 
     # Список ответственных
     cursor.execute(
@@ -1857,6 +1711,7 @@ def update_payment():
                 t1.payment_number,  
                 SUBSTRING(t1.basis_of_payment, 1,70) AS basis_of_payment_short,
                 t1.basis_of_payment, 
+                t5.user_id,
                 t5.first_name,
                 t5.last_name,
                 SUBSTRING(t1.payment_description, 1,70) AS payment_description_short,
@@ -1980,10 +1835,12 @@ def update_payment():
     login_app.conn_cursor_close(cursor, conn)
 
     print('update_payment')
-    print(len(payment))
+    print(len(cost_items))
+    print(cost_items)
     print(payment['payment_number'])
-    print(payment[0])
+    print(payment[4])
     print(payment)
+
 
 
 
--- static/script/application_modal.js
+++ static/script/application_modal.js
@@ -1,40 +1,67 @@
-//function applicationModal() {
-//  // Send an AJAX request to the server to get the modal HTML
-//  var xhr = new XMLHttpRequest();
-//  xhr.open('GET', '/modal', true);
-//  xhr.onreadystatechange = function() {
-//    if (xhr.readyState === 4 && xhr.status === 200) {
-//      // Create a new div element and set its innerHTML to the modal HTML
-//      var modalDiv = document.createElement('div');
-//      modalDiv.innerHTML = xhr.responseText;
-//
-//      // Append the modal div to the document body
-//      document.body.appendChild(modalDiv);
-//
-//      // Add event listener to the submit form
-//      var submitForm = document.getElementById('submit-form');
-//      submitForm.addEventListener('submit', function(event) {
-//        event.preventDefault(); // Prevent the form from submitting normally
-//
-//        // Get the data from the form input
-//        var data = submitForm.elements['data'].value;
-//
-//        // Send an AJAX request to the server to process the data
-//        var xhr = new XMLHttpRequest();
-//        xhr.open('POST', '/get_modal_payment', true);
-//        xhr.setRequestHeader('Content-Type', 'application/json');
-//        xhr.onreadystatechange = function() {
-//          if (xhr.readyState === 4 && xhr.status === 200) {
-//            // Handle the server response here
-//          }
-//        };
-//        xhr.send(JSON.stringify({ data: data }));
-//
-//        // Remove the modal window
-//        document.body.removeChild(modalDiv);
-//      });
-//    }
-//  };
-//  xhr.send();
-//});
-//}
+function getModal2(paymentId=null) {
+    var dataToUpdate = {
+        paymentId: paymentId,
+        newPaymentStatus: 'Completed'
+      };
+
+    // Send an AJAX request to the Flask route
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/update_payment', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            // Handle the response here
+            var updatedData = JSON.parse(xhr.responseText);
+
+            // Update the window with the new data
+            // Название карточки
+            document.getElementById('card_title').innerText = updatedData.payment[5];
+
+            // Наименование платежа
+            document.getElementById('basis_of_payment').innerText = updatedData.payment[6];
+
+            // Ответственный
+            var selectElement = document.getElementById('responsible')
+            selectElement.innerHTML = "";
+            // Loop through the new list
+            for (var i = 0; i < updatedData.responsible.length; i++) {
+                // Create a new option element
+                var option = document.createElement("option");
+                // Set the value and text of the option
+                option.value = updatedData.responsible[i][0];
+                option.text = updatedData.responsible[i][1] + ' ' + updatedData.responsible[i][2];
+                // Append the option to the select element
+                selectElement.appendChild(option);
+            }
+            document.getElementById('responsible').value = updatedData.payment[7]
+
+            // Тип заявки
+            var selectElement = document.getElementById('cost_items')
+            selectElement.innerHTML = "";
+            // Loop through the new list
+            for (var i = 0; i < updatedData.cost_items.length; i++) {
+                // Create a new option element
+                console.log(updatedData.cost_items[i][0], updatedData.cost_items[i][1], updatedData.cost_items[i][2])
+                var optgroup = document.createElement("optgroup");
+                for (var i2 = 0; i2 < updatedData.cost_items[i].length; i2++) {
+                    var option = document.createElement("option");
+                // Set the value and text of the option
+                option.value = updatedData.cost_items[i][0];
+                option.text = updatedData.cost_items[i][1] + ' ' + updatedData.responsible[i][2];
+                // Append the option to the select element
+                selectElement.appendChild(option);
+            }
+            document.getElementById('cost_items').value = updatedData.payment[9] + ' ' + updatedData.payment[8]
+            selectElement.selectedIndex = updatedData.payment[7]
+
+
+//            document.getElementById('responsible').innerText = updatedData.payment[6];
+            console.log(updatedData.payment[5]);
+            console.log(updatedData.payment);
+        }
+    };
+
+    xhr.send(JSON.stringify(dataToUpdate));
+
+};
