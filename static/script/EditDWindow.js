332630326a5c1f2e27a958554d81d7662c35853c
Tuleninov Roman <tuleninovroman@icloud.com>
2023-09-18 17:58:07 +0300

    fix: dwindow in payment-approval page

---
+++ static/script/EditDWindow.js
@@ -0,0 +1,18 @@
+
+const element = document.querySelector(".showModalId");
+const crossButton = document.querySelector("#crossBtn");
+const dialog = document.querySelector("#payment-approval__dialog");
+
+
+var elements = document.getElementsByClassName("showModalId");
+for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener("click", function () {
+        var dialog = document.getElementById("payment-approval__dialog");
+        dialog.showModal();
+    });
+}
+
+crossButton.addEventListener("click", closeDialog);
+function closeDialog() {
+    dialog.close();
+}
