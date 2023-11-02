501f33f50428893fd04248bb9e06d6883eea55f4
Tuleninov Roman <tuleninovroman@icloud.com>
2023-11-02 17:50:26 +0300

    dfds

--- static/script/todayYear.js
+++ static/script/todayYear.js
@@ -1,3 +1,10 @@
 let today = new Date();
 let year = today.getFullYear();
 document.getElementById("yearToday").textContent = year;
+
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+    }
+});
