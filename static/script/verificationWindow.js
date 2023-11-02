d9d70e3aee388c7f1e20339e8c58689e4af373e2
Tuleninov Roman <tuleninovroman@icloud.com>
2023-11-02 17:23:07 +0300

    dfgdfd

--- static/script/verificationWindow.js
+++ static/script/verificationWindow.js
@@ -4,7 +4,8 @@ const cancelDialogBtn = document.getElementById('verification_dialog__cancel');
 const nextDialogBtn = document.getElementById('verification_dialog__next');
 
 const paymentForm = document.getElementById('paymentForm');
-const dsubmit_button_in_form = document.getElementById('submitButton');
+const submit_button_in_form = document.getElementById('submitButton');
+
 
 const annulEditBtn = document.getElementById('annul__edit_btn_i');
 const annulApprovalEditBtn = document.getElementById('annul_approval__edit_btn_i');
@@ -16,12 +17,12 @@ const hideDialog = () => {
 };
 cancelDialogBtn.addEventListener('click', hideDialog);
 
-
 const showDialogDSB = () => {
     verificationDialog.showModal();
     paragraphQuestion.id = 'pqDSB'
 };
-dsubmit_button_in_form.addEventListener('click', showDialogDSB);
+submit_button_in_form.addEventListener('click', showDialogDSB);
+
 const showDialogAEB = () => {
     verificationDialog.showModal();
     paragraphQuestion.id = 'pqAEB'
@@ -40,8 +41,10 @@ saveEditBtn.addEventListener('click', showDialogSEB);
 
 
 nextDialogBtn.addEventListener('click', function () {
+
     if (paragraphQuestion.id === 'pqDSB') {
         paymentForm.submit();
+
     } else if (paragraphQuestion.id === 'pqAEB') {
         annulPayment();
     } else if (paragraphQuestion.id === 'pqAPEB') {
