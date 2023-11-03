064777d97aeb45d9945fad4409c4bd606cc153e1
dut1k <nikola.88@list.ru>
2023-11-03 10:06:02 +0300

    7

--- static/script/verificationWindow.js
+++ static/script/verificationWindow.js
@@ -6,55 +6,43 @@ const nextDialogBtn = document.getElementById('verification_dialog__next');
 const paymentForm = document.getElementById('paymentForm');
 const submit_button_in_form = document.getElementById('submitButton');
 
-
 const annulEditBtn = document.getElementById('annul__edit_btn_i');
 const annulApprovalEditBtn = document.getElementById('annul_approval__edit_btn_i');
 const saveEditBtn = document.getElementById('save__edit_btn_i');
 
-console.log(saveEditBtn)
-
-
 const hideDialog = () => {
     verificationDialog.close();
 };
-cancelDialogBtn.addEventListener('click', hideDialog);
 
 const showDialogDSB = () => {
     verificationDialog.showModal();
     paragraphQuestion.id = 'pqDSB'
 };
-submit_button_in_form.addEventListener('click', showDialogDSB);
 
 const showDialogAEB = () => {
     verificationDialog.showModal();
     paragraphQuestion.id = 'pqAEB'
 };
-try {
-    annulEditBtn.addEventListener('click', showDialogAEB);
-    const showDialogAPEB = () => {
-        verificationDialog.showModal();
-        paragraphQuestion.id = 'pqAPEB'
-    };
-} catch (err) {
 
-}
-try {
-    annulApprovalEditBtn.addEventListener('click', showDialogAPEB);
-    const showDialogSEB = () => {
-        verificationDialog.showModal();
-        paragraphQuestion.id = 'pqSEB'
-    };
-} catch (err) {
+const showDialogAPEB = () => {
+    verificationDialog.showModal();
+    paragraphQuestion.id = 'pqAPEB'
+};
 
-}
+const showDialogSEB = () => {
+    verificationDialog.showModal();
+    paragraphQuestion.id = 'pqSEB'
+};
 
-try {
-    console.log(22)
-    saveEditBtn.addEventListener('click', showDialogSEB);
-    console.log(12)
-} catch (err) {
+submit_button_in_form? submit_button_in_form.addEventListener('click', showDialogDSB): 1;
 
-}
+cancelDialogBtn? cancelDialogBtn.addEventListener('click', hideDialog): 1;
+
+annulEditBtn? annulEditBtn.addEventListener('click', showDialogAEB): 1;
+
+annulApprovalEditBtn? annulApprovalEditBtn.addEventListener('click', showDialogAPEB): 1;
+
+saveEditBtn? saveEditBtn.addEventListener('click', showDialogSEB): 1;
 
 nextDialogBtn.addEventListener('click', function () {
 
